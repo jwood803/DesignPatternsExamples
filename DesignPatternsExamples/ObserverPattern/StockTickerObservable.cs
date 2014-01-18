52e382e8c9c125b1f18da9f7ff7d593f32c0e529
Jon <jwood803@users.noreply.github.com>
2014-01-17 22:09:27 -0500

    Small code cleanup
    Moved the Unsubscriber class to its own file.

--- DesignPatternsExamples/ObserverPattern/StockTickerObservable.cs
+++ DesignPatternsExamples/ObserverPattern/StockTickerObservable.cs
@@ -48,24 +48,4 @@ namespace ObserverPattern
             return new Unsubscriber(_observers, observer);
         }
     }
-
-    public class Unsubscriber : IDisposable
-    {
-        private readonly List<IObserver<Stock>> _observers;
-        private readonly IObserver<Stock> _observer;
-
-        public Unsubscriber(List<IObserver<Stock>> observers, IObserver<Stock> observer)
-        {
-            _observers = observers;
-            _observer = observer;
-        }
-
-        public void Dispose()
-        {
-            if (_observer != null && _observers.Contains(_observer))
-            {
-                _observers.Remove(_observer);
-            }
-        }
-    }
 }
---
+++ DesignPatternsExamples/ObserverPattern/Unsubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    public class Unsubscriber : IDisposable
+    {
+        private readonly List<IObserver<Stock>> _observers;
+        private readonly IObserver<Stock> _observer;
+
+        public Unsubscriber(List<IObserver<Stock>> observers, IObserver<Stock> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+            {
+                _observers.Remove(_observer);
+            }
+        }
+    }
+}
